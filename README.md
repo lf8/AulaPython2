10427c0be3f5eb8957d976ff515145f50bdd09a6
lf8 <luis@Luiss-MacBook-Pro.local>
2017-09-02 17:08:53 -0300

    Aula de hoje e mais funcoes do Args

---
+++ README.md
@@ -0,0 +1,14 @@
+# AulaPython2
+
+Aula de hoje, criar um Command Line que ia interagir com o Docker
+
+docker-cli.py -h
+usage: docker-cli.py [-h] {criar,ls,procurar,rm} ...
+
+Docker-cli criado na aula de python
+
+positional arguments:
+  {criar,ls,procurar,rm}
+
+optional arguments:
+  -h, --help            show this help message and exit
---
+++ docker-cli.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python3
+
+import docker
+import argparse
+
+#client = docker.from_env()
+#get_all = client.containers.list(all)
+
+def criar_container(args):
+    client = docker.from_env()
+    executando = client.containers.run(args.imagem, args.comando)
+    print(executando)
+    return(executando)
+
+def listar_containers(args):
+    client = docker.from_env()
+    get_all = client.containers.list(all)
+    for cada_container in get_all:
+        conectando = client.containers.get(cada_container.id)
+        print ("O container da vez é o %s e ele utiliza a imagem %s e o comando %s" %(conectando.short_id, conectando.attrs['Config']['Image'], conectando.attrs['Config']['Cmd']))
+
+def procurar_container(args):
+    client = docker.from_env()
+    get_all = client.containers.list(all)
+    for cada_container in get_all:
+        conectando = client.containers.get(cada_container.id)
+        imagem_container = conectando.attrs['Config']['Image'].lower()
+        if str(args.imagem) in str(imagem_container):
+	#if set('alpine').intersection(conectando.attrs['Config']['Image'].lower()):
+            print("Achei o container %s que contem a palavra %s no nome de sua imagem" %(conectando.short_id, conectando.attrs['Config']['Image']))
+
+def remover_container(args):
+    #remover containers bindando abaixo de 1024
+    client = docker.from_env()
+    get_all = client.containers.list(all)
+    for cada_container in get_all:
+        conectando = client.containers.get(cada_container.id)
+        portas = conectando.attrs['HostConfig']['PortBindings']
+        if isinstance(portas,dict):
+            for porta, porta1 in portas.items():
+                porta1 = str(porta1)
+                porta2 = ''.join(filter(str.isdigit, porta1))
+                if int(porta2) <= 1024:
+                    print("Removendo o container %s que esta escutando na porta %s e bindando no host %s" %(str(cada_container.short_id) , str(porta1), str(porta2)))
+                    removendo = cada_container.remove(force=True)
+                    return(removendo)
+
+parser = argparse.ArgumentParser(description="Docker-cli criado na aula de python")
+subparser = parser.add_subparsers()
+
+#criar
+criar_opt = subparser.add_parser('criar')
+criar_opt.add_argument('--imagem', required=True)
+criar_opt.add_argument('--comando', required=True)
+criar_opt.set_defaults(func=criar_container)
+
+#listar
+listar_opt = subparser.add_parser('ls')
+listar_opt.set_defaults(func=listar_containers)
+
+#Procurar
+procurar_opt = subparser.add_parser('procurar')
+procurar_opt.add_argument('--imagem', required=True)
+procurar_opt.set_defaults(func=procurar_container)
+
+#remover_container
+remover_opt = subparser.add_parser('rm')
+remover_opt.set_defaults(func=remover_container)
+
+cmd = parser.parse_args()
+cmd.func(cmd)
